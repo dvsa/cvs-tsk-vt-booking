ee452fb8b5d851b8c7cfe56c6c52f1422f7be946
ian-hodges <ian.hodges@bjss.com>
2022-09-02 19:03:11 +0100

    refactor(cb2-5377): alter object initialization style

--- src/vehicleBooking/vehicleBooking.ts
+++ src/vehicleBooking/vehicleBooking.ts
@@ -10,17 +10,19 @@ export const vehicleBooking = {
 
     const vehicle = await vehicleDb.get(vtBooking.vrm);
 
-    const booking = new VehicleBooking();
-    booking.SHORT_NAME = vtBooking.name;
-    booking.VEHICLE_CLASS = vehicle.VEHICLE_CLASS;
-    booking.NO_OF_AXLES = vehicle.NO_OF_AXLES;
-    booking.TIMESTAMP0 = new Date(vtBooking.bookingDate);
-    booking.VRM = vtBooking.vrm;
-    booking.FK_APPTYP_APPL_TYP = vtBooking.testCode;
-    booking.FK_LANTBD_DATE = new Date(vtBooking.testDate);
-    booking.FK_STATN_ID = vtBooking.pNumber;
-    booking.FK_VEH_SYST_NO = vehicle.SYSTEM_NUMBER;
-    booking.COUNTED_AXLES = vehicle.NO_OF_AXLES;
+    const booking = {
+      ...new VehicleBooking(),
+      SHORT_NAME: vtBooking.name,
+      VEHICLE_CLASS: vehicle.VEHICLE_CLASS,
+      NO_OF_AXLES: vehicle.NO_OF_AXLES,
+      TIMESTAMP0: new Date(vtBooking.bookingDate),
+      VRM: vtBooking.vrm,
+      FK_APPTYP_APPL_TYP: vtBooking.testCode,
+      FK_LANTBD_DATE: new Date(vtBooking.testDate),
+      FK_STATN_ID: vtBooking.pNumber,
+      FK_VEH_SYST_NO: vehicle.SYSTEM_NUMBER,
+      COUNTED_AXLES: vehicle.NO_OF_AXLES,
+    };
 
     await vehicleBookingDb.insert(booking);
 
