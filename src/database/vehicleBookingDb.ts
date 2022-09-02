b191808972a98de0a6254c943e534953a3287dfb
ian-hodges <ian.hodges@bjss.com>
2022-09-02 16:15:57 +0100

    feat(cb2-5377): add missing await

--- src/database/vehicleBookingDb.ts
+++ src/database/vehicleBookingDb.ts
@@ -9,9 +9,9 @@ export const vehicleBookingDb = {
       "COALESCE((SELECT MAX(FK_BKGHDR_NO) + 1 from VEHICLE_BOOKING where FK_BKGHDR_USER_NO = 'XR'), 1)",
     );
 
-    const insertResult: string[] = connection
+    const insertResult: string[] = await connection
       .insert([vehicleBooking], ['FK_BKGHDR_NO'])
-      .into('VEHICLE_BOOKING') as unknown as string[];
+      .into('VEHICLE_BOOKING');
 
     if (insertResult.length === 0) {
       throw new Error('Insert failed. No data returned.');
