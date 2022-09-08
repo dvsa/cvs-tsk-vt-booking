2f11bb9ca9479eca6592a7447effc64da9c1b499
ian-hodges <ian.hodges@bjss.com>
2022-09-08 11:25:05 +0100

    feat(cb2-5777): remove sudo from getOracleClient.sh

--- scripts/getOracleClient.sh
+++ scripts/getOracleClient.sh
@@ -1,5 +1,5 @@
 cp -r ../lib .
 cd lib
-sudo wget https://download.oracle.com/otn_software/linux/instantclient/214000/instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
-sudo unzip -j instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
-sudo rm instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
+wget https://download.oracle.com/otn_software/linux/instantclient/214000/instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
+unzip -j instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
+rm instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
