9b5fc7731c3592344d569afc050b333ffd118819
Ian Hodges <ian.hodges@bjss.com>
2022-09-12 07:50:20 +0100

    feat(cb2-5777): add oracle binaries during package

---
+++ scripts/getOracleClient.sh
@@ -0,0 +1,5 @@
+cp -r ../lib .
+cd lib
+wget https://download.oracle.com/otn_software/linux/instantclient/214000/instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
+unzip -j instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
+rm instantclient-basiclite-linux.x64-21.4.0.0.0dbru.zip
